65b6449df8157841e313dc32639d3435f3288cd5
Jacob <jakebauer21@gmail.com>
2023-07-28 17:29:13 -0500

    started to use state

--- src/Content.js
+++ src/Content.js
@@ -1,15 +1,39 @@
 import React from 'react';
-import './index';
+import './';
+import { useState } from 'react';
 
 const Content = () => {
+	const [name, setName] = useState('Jake');
+	const [count, setCount] = useState('0');
 	const handleNameChange = () => {
 		const names = ['Jake', 'John', 'Dave'];
 		const int = Math.floor(Math.random() * 3);
-		return names[int];
+		setName(names[int]);
 	};
+
+	const handleClick = () => {
+		setCount(count + 1);
+		console.log(count);
+	};
+	const handleClick2 = (name) => {
+		console.log(`${name} clicked`);
+	};
+	const handleClick3 = (e) => {
+		console.log(e.target.innerText);
+	};
+
 	return (
 		<main>
-			<p>Hello {handleNameChange()} !</p>
+			<p onDoubleClick={handleClick}>Hello {name} !!!</p>
+
+			<button onClick={handleNameChange}>Change Name</button>
+			<button onClick={handleClick}>NO Click Me</button>
+			<button
+				onClick={(e) => {
+					handleClick3(e);
+				}}>
+				ok Click Me
+			</button>
 		</main>
 	);
 };
