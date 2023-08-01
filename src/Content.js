ab752a554a95e09533c24b004f89d4c864a7fcc6
Jacob <jakebauer21@gmail.com>
2023-08-01 15:39:01 -0500

    added js props and prop drilling by making files for each component

--- src/Content.js
+++ src/Content.js
@@ -1,56 +1,21 @@
 import React from 'react';
 import './';
-import { useState } from 'react';
-import { FaTrashAlt } from 'react-icons/fa';
+import ItemList from './ItemList';
 
-const Content = () => {
-	const [items, setItems] = useState([
-		{
-			id: 1,
-			checked: false,
-			item: 'One half pound bag fo almounds',
-		},
-		{
-			id: 2,
-			checked: false,
-			item: 'Item 2',
-		},
-		{
-			id: 3,
-			checked: false,
-			item: 'Item 3',
-		},
-	]);
-
-	const handleCheck = (id) => {
-		const listItems = items.map((item) =>
-			item.id === id ? { ...item, checked: !item.checked } : item
-		);
-		setItems(listItems);
-		localStorage.setItem('shoppinglist', JSON.stringify(listItems));
-	};
-
+const Content = ({ items, handleCheck, handleDelete }) => {
 	return (
 		<main>
-			<ul>
-				{items.map((item) => (
-					<li className="item" key={item.id}>
-						<input
-							type="checkbox"
-							onChange={() => handleCheck(item.id)}
-							checked={item.checked}
-						/>
-						<label onDoubleClick={() => handleCheck(item.id)}>
-							{item.item}
-						</label>
-						<FaTrashAlt role="button" tabIndex="0" />
-					</li>
-				))}
-			</ul>
+			{items.length ? (
+				<ItemList
+					items={items}
+					handleCheck={handleCheck}
+					handleDelete={handleDelete}
+				/>
+			) : (
+				<p style={{ marginTop: '2rem' }}>Empty</p>
+			)}
 		</main>
 	);
 };
-
-// min 24.49
 
 export default Content;
